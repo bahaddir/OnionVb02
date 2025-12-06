e349bde83ab7a88a5f92ea21296baefc542b5d75
bahad <bahad@BAHADIR>
2025-12-06 14:49:19 +0300

    ValidatorStructor

---
+++ OnionVb02.ValidatorStructor/Validators/CategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using OnionVb02.Application.DTOClasses;
+
+namespace OnionVb02.ValidatorStructor.Validators
+{
+    public class CategoryValidator : AbstractValidator<CategoryDto>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Ürün adı boş olamaz.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description boş olamaz.");
+        }
+    }
+
+}
---
+++ OnionVb02.ValidatorStructor/Validators/OrderDetailValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using OnionVb02.Application.DTOClasses;
+
+namespace OnionVb02.ValidatorStructor.Validators
+{
+    public class OrderDetailValidator : AbstractValidator<OrderDetailDto>
+    {
+        public OrderDetailValidator()
+        {
+            RuleFor(x => x.OrderId).NotEmpty().WithMessage("OrderId boş olamaz.");
+            RuleFor(x => x.ProductId).NotEmpty().WithMessage("ProductId boş olamaz.");
+        }
+    }
+
+}
